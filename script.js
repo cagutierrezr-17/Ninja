bd36b99667bf19a4063d12dd7ef91e7fc0237bcc
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-09-27 22:28:08 -0300

    [TAREA] sensei
    Tarea Requerida - Super Ninja

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ const mostrarEstadistica = new Ninja("Hyabusa", 0, 3, 3)
 
 console.log(nameNinja.decirNombre());
 console.log(mostrarEstadistica.mostrarEstadistica());
+console.log(mostrarEstadistica.tomarSake());
+
+class Maestro extends Ninja {
+    constructor(nombre, salud, velocidad, fuerza) {
+        super(nombre, salud, velocidad, fuerza)
+        this.sabiduria = 10;
+    }
+    hablarDeSabiduria() {
+        super.tomarSake();
+        return `Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.`;
+    }
+    mostrarEstadistica() {
+        return `Estadisticas del Ninja / Nombre: ${this.nombre} Salud: ${this.salud} Velocidad: ${this.velocidad} Fuerza: ${this.fuerza} Sabiduria: ${this.sabiduria}`;
+    }
+}
+
+const superSensei = new Maestro("Maestro Splinter", 200, 10, 10);
+
+console.log(superSensei.hablarDeSabiduria());
+console.log(superSensei.mostrarEstadistica());
