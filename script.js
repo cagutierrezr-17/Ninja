5b96b77ae06ae84ee609067612aaae0a6f8dfcca
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-09-27 22:24:40 -0300

    [TAREA] sensei
    Tarea Requerida - Super Ninja

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 class Ninja {
-    constructor(nombre, salud) {
+    constructor(nombre, salud, velocidad, fuerza) {
         this.nombre = nombre;
         this.salud = salud;
-        this.velocidad = 3;
-        this.fuerza = 3;
+        this.velocidad = velocidad;
+        this.fuerza = fuerza;
     }
     decirNombre() {
         return `El nombre del ninja es ${this.nombre}`
     }
-
     mostrarEstadistica() {
         return `Estadisticas del Ninja / Nombre: ${this.nombre} Fuerza: ${this.fuerza} Velocidad: ${this.velocidad} Salud ${this.salud}`
     }
-
     tomarSake() {
         this.salud += 10;
         return `Que rico Sake me hace sentir ${this.salud} veces mas saludable`
     }
-
 }
 
 const nameNinja = new Ninja("Hyabusa")
-const mostrarEstadistica = new Ninja("Hyabusa", 0)
-
-
+const mostrarEstadistica = new Ninja("Hyabusa", 0, 3, 3)
 
 console.log(nameNinja.decirNombre());
 console.log(mostrarEstadistica.mostrarEstadistica());
-console.log(mostrarEstadistica.tomarSake());
+console.log(mostrarEstadistica.tomarSake());        this.sabiduria = 10;
